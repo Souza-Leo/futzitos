f1979a3f9a3e445c9ba4c87af08e32ae2e710cc6
Raul dos Santos Moraes <48498755+rauldosS@users.noreply.github.com>
2024-09-29 18:25:08 -0300

    Create detail view

---
+++ player/views/player_list_view.py
@@ -0,0 +1,13 @@
+from django.views.generic.base import TemplateView
+
+from player.models import Player
+
+
+class PlayerListView(TemplateView):
+    template_name = 'player/player-list.html'
+    model = Player
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['players'] = self.model.objects.all()
+        return context
